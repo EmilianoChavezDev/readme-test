8e45d12f65148b666a7576fe09f44a43cf0b4875
Cristhian Marecos <cristhianmareccos2222@gmail.com>
2024-04-15 23:06:37 -0400

    Book chapters code refractor

--- cypress/e2e/tests/books/chapters/continue-chapter.cy.js
+++ cypress/e2e/tests/books/chapters/continue-chapter.cy.js
@@ -32,21 +32,17 @@ describe("Continue chapter", () => {
   });
 
   it("Continue writing chapter", () => {
-    // Vamos al boton crear libro
     Logger.stepNumber(3);
     Logger.step("Click en Escribe y en Crear libro nuevo del navbar");
     NavBarMethods.goToWriteBook();
 
-    // Verificamos que la url sea de creacion de libro
     Logger.verification("La url deberia ser la de creacion de libro");
-    cy.url().should("eq", CommonPageData.appPages.baseUrl + "books/create");
+    cy.url().should("eq", `${CommonPageData.appPages.baseUrl}books/create`);
 
-    // Interceptamos la peticion para simular que se creo el libro
     Logger.stepNumber(4);
     Logger.step("Llenamos el formulario y damos click en Seguir");
     cy.intercept("POST", CommonPageData.endPoints.books).as("createBook");
 
-    // Metodo para crear un libro y enviar la peticion
     CreateBookMethods.createBook(
       CreateBookData.bookData.title,
       CreateBookData.bookData.synopsis,
@@ -54,62 +50,50 @@ describe("Continue chapter", () => {
       CreateBookData.bookData.cover
     );
 
-    // Esperamos a que la peticion se complete
     cy.wait("@createBook").then((interception) => {
       expect(interception.response.statusCode).to.equal(201);
       bookId = interception.response.body.id;
 
-      // Salimos de la pagina de escribir capitulo
       Logger.stepNumber(5);
       Logger.step("Damos click para volver a la pagina de inicio");
       CreateBookMethods.cancelButtonClick();
 
-      // Verificamos que estemos en la pagina de inicio
       Logger.verification("Estamos en la pagina de inicio");
       cy.url().should("eq", CommonPageData.appPages.baseUrl);
 
-      // Vamos a la pagina de mis libros
       Logger.stepNumber(6);
       Logger.step("Vamos a la pagina de mis libros");
       NavBarMethods.goToMyBooks();
 
-      // Verificamos que estemos en la pagina de mis libros
       Logger.verification("Estamos en la pagina de mis libros");
       cy.url().should("eq", `${CommonPageData.appPages.baseUrl}books/mybooks`);
 
-      // Seleccionamos el libro y damos en seguir escribiendo
       Logger.stepNumber(7);
       Logger.step("Seleccionamos el libro y damos click en seguir escribiendo");
       MyBooksMethods.continueWritingClick();
 
-      // Verificamos que estemos en la pagina de escribir capitulo
       Logger.verification("Estamos en la pagina de escribir capitulo");
       cy.url().should(
         "eq",
         `${CommonPageData.appPages.baseUrl}books/${bookId}/chapters/write`
       );
 
-      // Insertamos el titulo
       Logger.stepNumber(8);
       Logger.step("Insertamos el titulo del capitulo");
       ChaptersMethods.insertTitle(ChaptersData.chapterContent.title);
 
-      // Insertamos el contenido del capitulo
       Logger.stepNumber(9);
       Logger.step("Insertamos el contenido del capitulo");
       ChaptersMethods.insertContent(ChaptersData.chapterContent.content);
 
-      // Interceptamos la peticion
       cy.intercept("POST", CommonPageData.endPoints.chapters).as(
         "publishChapter"
       );
 
-      // Publicamos el capitulo
       Logger.stepNumber(10);
       Logger.step("Damos click en Guardar");
       ChaptersMethods.saveButtonClick();
 
-      // Verficamos que se guardo el capitulo
       Logger.verification("El capitulo ha sido guardado");
       ChaptersMethods.verifySavedChapter();
 
